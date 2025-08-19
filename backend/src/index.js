d648072cda35bd48337c3b4347d282b6b193c3cb
vantu <tulevan526@gmail.com>
2025-08-19 12:17:23 +0700

    chore(project): setup deployment config & serve frontend
    - Add root package.json with build/start scripts
    - Update backend/package.json with start script
    - Configure backend to serve static files & index.html for SPA routing
    - Adjust axios baseURL and useAuthStore BASE_URL for prod/dev
    - Replace Vite favicon with custom logo.png and update title
    - Add project_deploy.txt with deployment instructions

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,13 @@ import messageRoutes from "./routes/message.route.js";
 import cor from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+// path.resolve() <=> process.cwd(), khi chạy "npm run start" -> npm tìm vị trí file package.json -> trả về path nây
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -24,6 +28,20 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve toàn bộ file tĩnh (JS/CSS/assets) đã build bởi Vite tại frontend/dist
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // vì path-to-regex là 8.2.0 nên "*" hay "/*" ko còn hợp lệ (dùng lệnh npm ls express path-to-regexp) để check phiên bản
+
+  // Catch-all cho SPA routes NHƯNG loại trừ các route bắt đầu bằng /api/
+  // Regex ^(?!\/api\/).* = mọi đường dẫn KHÔNG khởi đầu bằng "/api/"
+  app.get(/^(?!\/api\/).*/, (req, res) => {
+    // Trả về index.html để client-side router render trang tương ứng
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // nếu dùng app thì express tự tạo 1 http server r gắn app vào để xử lý các request HTTP bình thường, còn server này đã đc cấu hình là http server gốc
 server.listen(PORT, () => {
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,8 @@ import toast from "react-hot-toast";
 import { useChatStore } from "./useChatStore.js";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
